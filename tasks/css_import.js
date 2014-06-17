6fed99e1bf1f1aca7e906dc88bc6155f93d6edc1
lackhurt <pangjn@yeah.net>
2014-06-17 10:53:42 +0800

    根据@import指令合并css文件并处理背景图片的路径问题

--- tasks/css_import.js
+++ tasks/css_import.js
@@ -1,50 +1,65 @@
 /*
- * css-import
- * https://github.com/lackhurt/gruntjs
+ * grunt-css-import
+ * https://github.com/lackhurt/grunt-css-import
  *
  * Copyright (c) 2014 lackhurt
  * Licensed under the MIT license.
  */
 
 'use strict';
+var util = require('css-import-util');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    grunt.registerMultiTask('css_import', 'Concat the css file by "@import".', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            punctuation: '.',
+            separator: ', '
+        });
 
-  grunt.registerMultiTask('css_import', 'Concat the css file by "@import".', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            var concatContent = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function(filepath) {
+                var content = grunt.file.read(filepath);
+                var relativeExtraCss = util.parseExtraCss(content);
+
+                grunt.log.subhead('Processing file "' + filepath + '" :');
+
+                var extraCssContentArr = relativeExtraCss.map(function(importPath) {
+                    if (util.isRelativeUrl(importPath)) {
+                        grunt.log.writeln('Found extra relative path file : ' + importPath);
+                        return util.replaceExtraResourcesPath(grunt.file.read(util.fetchImportPath(filepath, importPath)), importPath);
+                    } else {
+                        grunt.log.warn('Found absolute path file : ' + importPath);
+                        return '@import "' + importPath + '";';
+                    }
+                });
+
+                var splitedCssContentArr = util.splitCssFileByImport(content);
+
+                var resultArr = [];
+                // 按照src顺序合并文件
+                for (var i = 0; i < Math.max(splitedCssContentArr.length, extraCssContentArr.length); i++) {
+                    resultArr.push(splitedCssContentArr.shift());
+                    resultArr.push(extraCssContentArr.shift());
+                }
+
+                return resultArr.filter(function(e) {
+                    return !!e;
+                }).join('\n');
+            });
+
+            grunt.file.write(f.dest, concatContent.join('\n'));
+            grunt.log.ok('File "' + f.dest + '" created.');
+        });
     });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
-
 };
